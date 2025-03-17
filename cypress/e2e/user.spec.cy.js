025612da833c969999bc2b98ce9cb92c9903bd2d
Leticia Menezes <dev.leticiamenezes@gmail.com>
2025-03-17 17:46:37 -0300

    add ramdom values on user spec chancejs

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,7 +4,9 @@ import DashboardPage from '../pages/dashboardPage'
 import MenuPage from '../pages/menuPage'
 import MyInfoPage from '../pages/myInfoPage'
 
+const Chance = require('chance')
 
+const chance = new Chance()
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
@@ -18,8 +20,8 @@ describe('Orange HRM Tests', () => {
     loginPage.checkAcessValid
     dashboardPage.checkDashboardPage()
     menuPage.accessMyInfo()
-    myInfoPage.fillPersonalDetails('First Name', 'Middle Name', 'Last Name')
-    myInfoPage.fillEmployeeDetails('employId', 'otehrId','Drivers Number', '2025-07-09')
+    myInfoPage.fillPersonalDetails(chance.first(), chance.last(), chance.last())
+    myInfoPage.fillEmployeeDetails(chance.string({ length: 5 }),chance.string({ length: 5 }), chance.string({ length: 5 }), '2025-07-09')
     myInfoPage.fillStatus('2025-08-08')
     myInfoPage.saveForm()
     
